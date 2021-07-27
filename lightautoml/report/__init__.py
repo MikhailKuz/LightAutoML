d145e3ae0e57b1a2b11d0926748cd33e307c59ae
Alexander Kirilin <adkirilin@gmail.com>
2021-07-27 16:35:33 +0300

    Tools

--- lightautoml/report/__init__.py
+++ lightautoml/report/__init__.py
@@ -1,5 +1,9 @@
 """Report generators and templates."""
 
 from .report_deco import ReportDeco, ReportDecoWhitebox, ReportDecoNLP
+from lightautoml.utils.installation import __validate_extra_deps
+
+__validate_extra_deps('pdf')
+
 
 __all__ = ['ReportDeco', 'ReportDecoWhitebox', 'ReportDecoNLP']
